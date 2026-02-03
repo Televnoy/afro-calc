da328f51eed56d2dcaea72c18c11004cad6632eb
Televnoy <vtelevnoy@gmail.com>
2026-02-03 22:47:40 +0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,20 @@
-﻿<!DOCTYPE html>
-<html>
-<head>
-	<title>be1.ru</title>
-</head>
-<body>
-<p>const CACHE_NAME = &#39;afroperm-v2&#39;;</p>
+const CACHE_NAME = 'afroperm-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
-<p>const ASSETS = [</p>
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
 
-<p>&nbsp; &#39;./&#39;,</p>
-
-<p>&nbsp; &#39;./index.html&#39;,</p>
-
-<p>&nbsp; &#39;./manifest.json&#39;</p>
-
-<p>];</p>
-
-<p>&nbsp;</p>
-
-<p>self.addEventListener(&#39;install&#39;, (event) =&gt; {</p>
-
-<p>&nbsp; event.waitUntil(</p>
-
-<p>&nbsp; &nbsp; caches.open(CACHE_NAME).then((cache) =&gt; cache.addAll(ASSETS))</p>
-
-<p>&nbsp; );</p>
-
-<p>});</p>
-
-<p>&nbsp;</p>
-
-<p>self.addEventListener(&#39;fetch&#39;, (event) =&gt; {</p>
-
-<p>&nbsp; event.respondWith(</p>
-
-<p>&nbsp; &nbsp; caches.match(event.request).then((response) =&gt; {</p>
-
-<p>&nbsp; &nbsp; &nbsp; return response || fetch(event.request);</p>
-
-<p>&nbsp; &nbsp; })</p>
-
-<p>&nbsp; );</p>
-
-<p>});</p>
-</body>
-</html>
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
