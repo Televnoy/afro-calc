f3ffc30c4ddd98c88f57f3824a315c758f514122
Televnoy <vtelevnoy@gmail.com>
2026-02-05 00:41:38 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,35 @@
-﻿const CACHE_NAME = 'afroperm-v2';
+﻿const CACHE_NAME = 'afroperm-cache-v1';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json'
 ];
 
+// Установка воркера и кэширование ресурсов
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+// Активация и удаление старого кэша
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
     })
   );
 });
+
+// Обработка запросов (сначала кэш, потом сеть)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+
